9ad8493d1ba863a5bae2da35825189d870328330
Cody <codyruggiero@gmail.com>
2022-02-11 12:11:31 -0500

    Mostly finishing up the tutorial now.

--- index.js
+++ index.js
@@ -5,31 +5,73 @@ const cheerio = require('cheerio'); // for parsing the html data
 const res = require('express/lib/response'); 
 const { response } = require('express');
 
+const newspapers = [
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.thetimes.co.uk/environment',
+        base: ''
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change/',
+        base: 'https://www.telegraph.co.uk'
+    }
+]
+
 const app = express();  // for more easily using express
 const PORT = process.env.PORT // express port
-const URL = process.env.URL // the url that's being parsed
 const articles = [] // the array that the data is going into
 
-app.get('/', (req, res) => { // the main page of the api. 
-    res.json('Welcome to my AJC API')
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        $('a:contains("climate")', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).attr('href')
+
+            articles.push({
+                title,
+                url: newspaper.base + url,
+                source: newspaper.name
+            })
+        })
+    }).catch(err => console.log(err))
 })
 
-app.get('/news', (req, res) =>{ // the page for the api for the articles
-    axios.get(`${URL}`)
-        .then((response) => {
-            const html = response.data;
-            const $ = cheerio.load(html);
+app.get('/news', (req, res) => { // the page for the api for the articles
+    res.json(articles)
+})
 
-            $('a:contains("COVID")', html).each(function () {
+app.get('/news/:newspaperId', async(req, res) => {
+    const newspaperId = req.params.newspaperId
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].base
+    axios.get(newspaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("climate")', html).each(function () {
                 const title = $(this).text()
                 const url = $(this).attr('href')
-                articles.push({
+                specificArticles.push({
                     title,
-                    url
+                    url: newspaperBase + url,
+                    source: newspaperId
                 })
             })
-            res.json(articles)
+            res.json(specificArticles)
         }).catch(err => console.log(err))
+    // axios.get()
 })
 
 app.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
